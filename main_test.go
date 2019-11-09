c4ef26d950fdbc74133c9083ac272ede8ff71795
Michael Onishchenko <painter@3lines.club>
2019-11-09 22:40:18 +0300

    the error messages were made better

--- main_test.go
+++ main_test.go
@@ -10,14 +10,14 @@ func TestCalc1(t *testing.T) {
 	_, x1, x2, _ := Calc(2, 10, 2)
 
 	if strconv.FormatFloat(x1, 'g', 6, 64) != "-4.79129" || strconv.FormatFloat(x2, 'g', 6, 64) != "-0.208712" {
-		t.Error("error 1")
+		t.Error("error 1, D > 0")
 	}
 }
 func TestCalc2(t *testing.T) {
 	D, _, _, _ := Calc(2, 4, 2)
 
 	if D != 0 {
-		t.Error("error 2")
+		t.Error("error 2, D = 0")
 	}
 
 }
@@ -26,7 +26,7 @@ func TestCalc3(t *testing.T) {
 	_, _, _, conclusion := Calc(2, 2, 2)
 
 	if conclusion != less0 {
-		t.Error("error 3")
+		t.Error("error 3, D < 0")
 	}
 
 }
