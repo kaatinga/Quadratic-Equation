deff3ced72508f7e2653f5e34e6a1416012d9b86
Michael Onishchenko <painter@3lines.club>
2019-11-09 22:40:54 +0300

    a small optimization so as to save some electricity

--- main.go
+++ main.go
@@ -21,8 +21,9 @@ func Calc(a, b, c float64) (D, x1, x2 float64, conclusion string) {
 		x2 = x1
 		conclusion = strings.Join([]string{"x1 = x2 = ", strconv.FormatFloat(x1, 'g', 6, 64)}, "")
 	case D > 0:
-		x1 = (-b - math.Sqrt(D)) / 2 / a
-		x2 = (-b + math.Sqrt(D)) / 2 / a
+		sqrtD := math.Sqrt(D)
+		x1 = (-b - sqrtD) / 2 / a
+		x2 = (-b + sqrtD) / 2 / a
 		conclusion = strings.Join([]string{"x1 = ", strconv.FormatFloat(x1, 'g', 6, 64), ", x2 = ", strconv.FormatFloat(x2, 'g', 6, 64)}, "")
 	}
 
